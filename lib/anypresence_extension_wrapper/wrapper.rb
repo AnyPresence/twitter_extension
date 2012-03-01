a843d6b5031d959b93469673ea9b6465c258a84e
Khem Veasna <kveasna@anypresence.com>
2012-03-01 09:41:40 -0500

    Added functionality to obtain available object definitions.

---
+++ lib/anypresence_extension_wrapper/wrapper.rb
@@ -0,0 +1,46 @@
+module AnypresenceExtensionWrapper
+  class Wrapper
+    def initialize(account_id)
+      @account = AnypresenceExtension::Account.find_by_id(account_id)
+    end
+    
+    # Build an AnypresenceExtension client.
+    def ap_client(*adhoc_api_version)
+      version = adhoc_api_version.empty? ? 'latest' : adhoc_api_version[0]
+      @ap_client ||= AnypresenceExtension::Client.new(@account.api_host, @account.api_token, @account.application_id, version)
+    end
+    
+    # Gets object definition metadata using API call.
+    def object_definition_metadata
+      version = @account.api_version.nil? ? 'latest' : @account.api_version
+      response = ap_client(version).metadata.fetch
+
+      Rails.logger.info "response: " + response.inspect
+      parsed_json = []
+      case response.status
+      when 200
+        begin
+          parsed_json = ActiveSupport::JSON.decode(response.body)
+        rescue MultiJson::DecodeError
+          raise "Unable to decode the JSON message."
+        end
+      else
+        raise "Unable to get a response."
+      end
+
+      parsed_json
+    end
+
+    # Gets object definition names for an application
+    def object_definition_mappings
+      parsed_json = object_definition_metadata
+
+      object_names = []
+      parsed_json.each do |object_definition|
+        object_names << object_definition["name"].downcase
+      end
+      object_names
+    end
+  end
+ 
+end
