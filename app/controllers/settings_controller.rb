28dd240c1e58b221b916dafa6653cba0823be464
Khem Veasna <kveasna@anypresence.com>
2012-03-01 08:51:55 -0500

    Tied twitter key fields to the UI.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,27 +2,34 @@ class SettingsController < AnypresenceExtension::SettingsController
   layout 'application'
   
   def settings
-    settings!
+    if request.put?
+      if current_account.update_attributes params[:account], :without_protection => true
+        flash[:notice] = "Account updated."
+        redirect_to settings_path
+      else
+        flash[:alert] = "Account could not be updated."
+
+        render :action => "settings", :controller => "settings"
+      end
+    end
   end
 
   def perform
-   Twitter.configure do |config|
-     config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-     config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-     config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
-     config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
-   end
+    Twitter.configure do |config|
+      config.consumer_key = current_account.twitter_consumer_key.strip
+      config.consumer_secret = current_account.twitter_consumer_secret.strip
+      config.oauth_token = current_account.twitter_oauth_token.strip
+      config.oauth_token_secret = current_account.twitter_oauth_token_secret.strip
+    end
    
-   begin
-     debugger
-     ret = Twitter.update("I'm tweeting with @gem!!")
-     Rails.logger.info "twitter response: " + ret.inspect
-     render :json => { :success => true }
-   rescue
-     Rails.logger.error "Unable to send tweet: " + $!.message
-     render :json => { :success => false }
-   end
-
+    begin
+      ret = Twitter.update("I'm tweeting with @gem!!")
+      Rails.logger.info "twitter response: " + ret.inspect
+      render :json => { :success => true }
+    rescue
+      Rails.logger.error "Unable to send tweet: " + $!.message
+      render :json => { :success => false }
+    end
   end
   
 end
