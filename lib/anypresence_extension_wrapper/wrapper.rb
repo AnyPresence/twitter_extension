ba428746faef9083702eebb2b4c3cec3c11a71ed
Khem Veasna <kveasna@anypresence.com>
2012-03-02 09:51:56 -0500

    Tweaked comments.

--- lib/anypresence_extension_wrapper/wrapper.rb
+++ lib/anypresence_extension_wrapper/wrapper.rb
@@ -4,13 +4,17 @@ module AnypresenceExtensionWrapper
       @account = AnypresenceExtension::Account.find_by_id(account_id)
     end
     
-    # Build an AnypresenceExtension client.
+    # Builds an AnypresenceExtension client for accessing the platform api.
+    #
+    # Returns a client
     def ap_client(*adhoc_api_version)
       version = adhoc_api_version.empty? ? 'latest' : adhoc_api_version[0]
       @ap_client ||= AnypresenceExtension::Client.new(@account.api_host, @account.api_token, @account.application_id, version)
     end
     
     # Gets object definition metadata using API call.
+    #
+    # Returns the parsed json response.
     def object_definition_metadata
       version = @account.api_version.nil? ? 'latest' : @account.api_version
       response = ap_client(version).metadata.fetch
@@ -32,6 +36,8 @@ module AnypresenceExtensionWrapper
     end
 
     # Gets object definition names for an application
+    #
+    # Returns an array of object names.
     def object_definition_mappings
       parsed_json = object_definition_metadata
 
