4d8652085837bbdb70f914c7eec98a80addbf777
Khem Veasna <kveasna@anypresence.com>
2012-06-01 09:40:15 -0400

    Scaffold outage model for spec/dummy

--- app/models/anypresence_extension/account.rb
+++ app/models/anypresence_extension/account.rb
@@ -1,8 +1,15 @@
 require AnypresenceExtension::Engine.root.join('app', 'models', 'anypresence_extension', 'account')
 
 module AnypresenceExtension 
-  class Account < ActiveRecord::Base
-    has_many :tweet_options, :dependent => :destroy
-  
+  class Account
+    include ActiveModel::MassAssignmentSecurity
+    include Mongoid::Document
+    include Mongoid::Timestamps
+    
+    field :twitter_consumer_key, type: String
+    field :twitter_consumer_secret, type: String
+    field :twitter_oauth_token, type: String
+    field :twitter_oauth_token_secret, type: String
+
   end
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :outages
+
   mount TwitterExtension::Engine => "/twitter_extension"
 end
